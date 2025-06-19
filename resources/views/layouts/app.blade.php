252762d9bb7b6899f963b72384333a4d392ed79c
kouheigit <ookawakouhei777@gmail.com>
2025-06-19 18:40:30 +0900

    Create index page with Blade to display post list

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>{{ config('app.name') }}</title>
+</head>
+<body>
+<div class="container">
+    @yield('content')
+</div>
+</body>
+</html>
+
+{{---
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>{{ config('app.name') }}</title>
+</head>
+<body>
+<div class="container">
+    @yield('content')
+</div>
+</body>
+</html>--}}
